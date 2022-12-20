94bc3607d108315705c5a85501005c8ca3f471b7
tildejustin <spacepepper3.14@gmail.com>
2022-12-20 16:03:46 -0500

    first commit

---
+++ run.js
@@ -0,0 +1,15 @@
+document.addEventListener("keypress", (e) => {
+    // console.log(e.code, e.key)
+    if (e.code == "Slash") {
+        if (/.*:\/\/jisho\.org\/.*/.test(window.location.href)) {
+            if (!document.getElementById("keyword").matches(":focus")) {
+                document.getElementById("keyword").focus();
+            }
+        } else if (/.*:\/\/(.*\.)?twitch\.tv\/.*/.test(window.location.href)) {
+            console.log("twitch activated");
+            if (!document.getElementsByClassName("tw-input")[0].matches(":focus")) {
+                document.getElementsByClassName("tw-input")[0].focus();
+            }
+        }
+    }
+});
