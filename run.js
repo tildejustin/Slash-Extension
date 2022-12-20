665ce7ad7de3ca9a59e6389da290868c44464a5b
tildejustin <spacepepper3.14@gmail.com>
2022-12-20 18:52:12 -0500

    v0.0.2

--- run.js
+++ run.js
@@ -1,12 +1,11 @@
 document.addEventListener("keypress", (e) => {
     // console.log(e.code, e.key)
     if (e.code == "Slash") {
-        if (/.*:\/\/jisho\.org\/.*/.test(window.location.href)) {
+        if (/.*:\/\/(.*\.)?jisho\.org\/.*/.test(window.location.href)) {
             if (!document.getElementById("keyword").matches(":focus")) {
                 document.getElementById("keyword").focus();
             }
         } else if (/.*:\/\/(.*\.)?twitch\.tv\/.*/.test(window.location.href)) {
-            console.log("twitch activated");
             if (!document.getElementsByClassName("tw-input")[0].matches(":focus")) {
                 document.getElementsByClassName("tw-input")[0].focus();
             }
