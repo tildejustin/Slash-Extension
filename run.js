d50de084371744f00e9a0373763246147a8e3e34
tildejustin <spacepepper3.14@gmail.com>
2022-12-22 14:05:30 -0500

    comments

--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 // template regex: /.*:\/\/(.*\.)?<site>\.<tld>\/.*/
-// should make a function that takes the site and generates the regex, but too lazy
+//TODO: should make a function that takes the site and generates the regex, but too lazy
 
 document.addEventListener("keypress", (e) => {
     // console.log(e.code, e.key)
-    // using e.code == "Slash" makes it trigger on "?" too.
+    // using e.code == "Slash" makes it trigger on "?" inputs too
     if (e.key == "/") {
         if (/.*:\/\/(.*\.)?jisho\.org\/.*/.test(window.location.href)) {
             if (document.getElementById("keyword").matches(":focus")) return
@@ -11,6 +11,7 @@ document.addEventListener("keypress", (e) => {
         } else if (/.*:\/\/(.*\.)?twitch\.tv\/.*/.test(window.location.href)) {
             if (document.getElementsByClassName("tw-input")[0].matches(":focus")) return
             // make sure chat isn't focused
+            // TODO: doesn't work with chatterino extension, need to make a check for that
             if (document.querySelectorAll("[data-slate-node]")[0].matches(":focus")) return
             document.getElementsByClassName("tw-input")[0].focus();
         }
