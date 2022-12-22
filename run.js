2a3823ef4014c9b7fb2ba8a90252bec84ebba14e
tildejustin <spacepepper3.14@gmail.com>
2022-12-21 20:49:47 -0500

    twitch doesn't switch focus when chat is focused

--- run.js
+++ run.js
@@ -1,14 +1,18 @@
+// template regex: /.*:\/\/(.*\.)?<site>\.<tld>\/.*/
+// should make a function that takes the site and generates the regex, but too lazy
+
 document.addEventListener("keypress", (e) => {
     // console.log(e.code, e.key)
-    if (e.code == "Slash") {
+    // using e.code == "Slash" makes it trigger on "?" too.
+    if (e.key == "/") {
         if (/.*:\/\/(.*\.)?jisho\.org\/.*/.test(window.location.href)) {
-            if (!document.getElementById("keyword").matches(":focus")) {
-                document.getElementById("keyword").focus();
-            }
+            if (document.getElementById("keyword").matches(":focus")) return
+            document.getElementById("keyword").focus();
         } else if (/.*:\/\/(.*\.)?twitch\.tv\/.*/.test(window.location.href)) {
-            if (!document.getElementsByClassName("tw-input")[0].matches(":focus")) {
-                document.getElementsByClassName("tw-input")[0].focus();
-            }
+            if (document.getElementsByClassName("tw-input")[0].matches(":focus")) return
+            // make sure chat isn't focused
+            if (document.querySelectorAll("[data-slate-node]")[0].matches(":focus")) return
+            document.getElementsByClassName("tw-input")[0].focus();
         }
     }
 });
